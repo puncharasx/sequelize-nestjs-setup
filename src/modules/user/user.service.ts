59b17e6a0f69d823f167732f8c6165b99c521ded
Puncharasx <puncharasx@gmail.com>
2023-03-23 00:12:54 +0700

    Initial commit

---
+++ src/modules/user/user.service.ts
@@ -0,0 +1,14 @@
+import { Inject, Injectable } from '@nestjs/common';
+import { User } from './user.entity';
+
+@Injectable()
+export class UserService {
+  constructor(
+    @Inject('USER_REPOSITORY')
+    private userRepository: typeof User,
+  ) {}
+
+  async findAll(): Promise<User[]> {
+    return this.userRepository.findAll<User>();
+  }
+}
